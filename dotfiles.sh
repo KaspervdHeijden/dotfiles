d990591f46cd7a7a451e85d838a5aac442a66d93
vanderheijdenk <vanderheijdenk@transip.nl>
2019-04-28 14:53:38 +0200

    Add default editor

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,6 @@
 export SHELL=$(which $(ps -p $$ -o 'comm='));
 export GIT_PAGER=more;
+export EDITOR=nano;
 
 if [[ ! -z "${BASH_SOURCE}" ]]; then
     export DOTFILES_DIR=$(dirname "${BASH_SOURCE}");
