a33f1e280e2c77f1186b596b67992896e04b6a48
vanderheijdenk <vanderheijdenk@transip.nl>
2019-04-30 17:33:41 +0200

    Remove --color=auto in lsa alias

--- aliases.sh
+++ aliases.sh
@@ -3,10 +3,10 @@
 
 alias tm="tmux attach -f ${DOTFILES_DIR}/tmux.conf 2> /dev/null || tmux -f ${DOTFILES_DIR}/tmux.conf new > /dev/null 2> /dev/null";
 alias shq="ps waux | grep ssh | grep -v grep | awk '{print $2}' | uniq | xargs -r kill";
-alias lsa='ls -lAFh --color=auto';
 alias gc='git checkout';
 alias gs='git status';
 alias rr='repo-root';
+alias lsa='ls -lAFh';
 alias gd='git diff';
 alias ga='git add';
 alias gitc='gitt';
