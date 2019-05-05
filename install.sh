3a687ccf61a0ee1621a4362018831c7ce6bdb553
vanderheijdenk <vanderheijdenk@transip.nl>
2019-05-05 13:05:25 +0200

    Use /bin/sh instead install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
-#!/bin/bash --
+#!/bin/sh --
 
 cur_dir=$(realpath $(dirname "${0}"));
 echo "Including dotfiles from ${cur_dir}";
 
-for file in ~/.bashrc ~/.bash_profile ~/.zshrc; do
-    if [[ -f "${file}" && ! $(grep 'DOTFILES_DIR=' "${file}") ]]; then
-        echo "Initialize dotfiles for '${file}'";
-
-        echo ''  >> "${file}";
-        echo '# Include dotfiles' >> "${file}";
-        echo "export DOTFILES_DIR='${cur_dir}';" >> "${file}";
-        if [[ $(echo "${file}" | grep 'bash') ]]; then
-            echo "source '${cur_dir}/bash/bashrc.sh';" >> "${file}";
-        else
-            echo "source '${cur_dir}/zsh/zshrc.sh';" >> "${file}";
-        fi
+for shell_name in bash zsh; do
+    file="$(echo ~)/.${shell_name}rc";
+    if $(grep 'DOTFILES_DIR=' "${file}"); then
+        continue;
     fi
+
+    echo "${shell_name}";
+    echo '# Include dotfiles' >> "${file}";
+    echo "export DOTFILES_DIR='${cur_dir}';" >> "${file}";
+    echo "source '${cur_dir}/${shell_name}/${shell_name}rc.sh';" >> "${file}";
 done;
 
 echo 'Done. Please restart your shell, and call repo-search';
