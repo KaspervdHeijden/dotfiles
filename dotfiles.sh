1f47b3614a6668af776d0156cff144d63af1f605
Kasper van der Heijden <vanderheijdenk@transip.nl>
2021-07-14 09:27:07 +0200

    Simpeler message

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,5 +10,5 @@ export PAGER=less;
 [ -f "${HOME}/.config/dotfiles/config.sh" ] && . "${HOME}/.config/dotfiles/config.sh";
 
 if [ -f "${DF_BIRTHDAYS_FILE}" ]; then
-    awk -v day="$(date +'%m-%d')" '$1 == day { print "!! birthday " $2 " today: " $3; print ""; }' "${DF_BIRTHDAYS_FILE}";
+    awk -v day="$(date +'%m-%d')" '$1 == day { print "Happy birthday " $3 "!"; }' "${DF_BIRTHDAYS_FILE}";
 fi
