fb216891492d8a4c05db3172ae5546ac93138e3b
Kasper van der Heijden <vanderheijdenk@transip.nl>
2021-08-23 12:36:26 +0200

    Add more completion options for ZSH

--- shells/zsh/rc.sh
+++ shells/zsh/rc.sh
@@ -4,12 +4,21 @@ case $- in
       *) return;;
 esac
 
-setopt prompt_subst histignoredups appendhistory interactivecomments;
-
 autoload -Uz compinit && compinit;
 autoload -U colors && colors;
 
 bindkey -e;
+
+unsetopt menu_complete;
+unsetopt flowcontrol;
+
+setopt prompt_subst;
+setopt histignoredups;
+setopt appendhistory;
+setopt interactivecomments;
+setopt auto_menu;
+setopt complete_in_word;
+setopt always_to_end;
 
 export DF_ROOT_DIR="$(realpath "$(dirname $0)/../../")";
 export HISTFILE="${HOME}/.zsh_history";
@@ -18,23 +27,8 @@ export SAVEHIST=20000;
 
 command -v dircolors >/dev/null && eval "$(dircolors -b)";
 
-zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*';
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s;
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s;
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31';
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd';
-zstyle ':completion:*' completer _expand _complete _correct _approximate;
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS};
-zstyle ':completion:*' auto-description 'specify: %d';
-zstyle ':completion:*' format 'Completing %d';
-zstyle ':completion:*' use-compctl false;
-zstyle ':completion:*' list-colors '';
-zstyle ':completion:*' group-name '';
-zstyle ':completion:*' menu select=2;
-zstyle ':completion:*' verbose true;
-
 if [ -d "${ZSH}" ] && [ -r "${ZSH}/oh-my-zsh.sh" ]; then
-    export DISABLE_UPDATE_PROMPT=1;
+    export DISABLE_UPDATE_PROMPT=true;
     export UPDATE_ZSH_DAYS=7;
     export plugins=(sudo pass);
 
